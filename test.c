729b41d607620632ca8fb2848e55af386306c169
Miguel Angel Garcia Beltran <migugar2@c2r10s3.42madrid.com>
2024-09-28 19:59:02 +0200

    Add some simple test

--- test.c
+++ test.c
@@ -6,12 +6,14 @@ int	main(void)
 {
 	char *str = "xd";
 
+	assert(ft_printf("") == printf(""));
 	assert(ft_printf("Hello, %c!\n", 'w') == printf("Hello, %c!\n", 'w'));
 	assert(ft_printf("x: %c\n", str[0]) == printf("x: %c\n", str[0]));
 	assert(ft_printf("d: %c\n", str[1]) == printf("d: %c\n", str[1]));
-	assert(ft_printf("Hello, %s!\n", str) == printf("Hello, %s!\n", str));
 	assert(ft_printf("Hello, world!\n") == printf("Hello, world!\n"));
+	assert(ft_printf("%s\n", "") == printf("%s\n", ""));
 	assert(ft_printf("Hello, %s!\n", "world") == printf("Hello, %s!\n", "world"));
+	assert(ft_printf("Hello, %s!\n", str) == printf("Hello, %s!\n", str));
 	assert(ft_printf("NULL as string: %s!\n", (char *)NULL) == printf("NULL as string: %s!\n", (char *)NULL));
 	assert(ft_printf("Hello, %d!\n", 42) == printf("Hello, %d!\n", 42));
 	assert(ft_printf("Inferior limit: %d!\n", -2147483648) == printf("Inferior limit: %d!\n", (int)-2147483648));
